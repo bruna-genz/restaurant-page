26e1921c8d47d1b30a65a0a4fcbd65b148a4819a
Bruna <brunagenz92@gmail.com>
2020-04-02 20:02:56 -0300

    refactor createOptions method and create a dinner menu

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,51 @@
-let contentWrapper;
-let menuWrapper;
-
 import * as Common from './common';
 import Pasta from './assets/images/pasta.jpg';
 import Ratatouille from './assets/images/ratatouille_ed.jpg';
 import Risotto from './assets/images/risotto.jpg';
+import Salmon from './assets/images/salmon.jpg';
+import Ribs from './assets/images/ribs.jpg';
+import Burger from './assets/images/burger.jpg';
+
+let contentWrapper;
+let menuWrapper;
+
+let lunchOptions = [
+    {
+        img: Pasta,
+        title: "Caponata pasta",
+        description: "Pasta Caponata is inspired by the Sicilian dish made with chopped eggplant, olives and tomatoes in a sweet and sour sauce. It's a light an easy meal that's vegan and gluten-free!"
+    },
+    {
+        img: Risotto,
+        title: "Seafood Risotto",
+        description: "Risotto is a northern Italian rice dish cooked with broth until it reaches a creamy consistency."
+    },
+    {
+        img: Ratatouille,
+        title: "Ratatouille",
+        description: "Ratatouille is a French Provençal stewed vegetable dish, originating in Nice, and sometimes referred to as ratatouille niçoise."
+    }
+]
+
+let dinnerOptions = [
+    {
+        img: Salmon,
+        title: "Roasted salmon",
+        description: "Tender Garlic Butter Baked Salmon with crispy roast potatoes, asparagus and a delicious garlic butter sauce."
+    },
+    {
+        img: Ribs,
+        title: "Pork ribs",
+        description: "Chef John's indoor baby back ribs are seasoned with a homemade dry rub, then glazed with barbeque sauce."
+    },
+    {
+        img: Burger,
+        title: "The Windsor's Burger",
+        description: "Beef patty, cheddar, pork belly, mushroom stuffed onion ring, frites, served with a delicious aioli saude."
+    }
+];
+
+
 export const setContentWrapper = _contentWrapper => contentWrapper = _contentWrapper;
 
 export const createMenu = () => {
@@ -12,34 +53,18 @@ export const createMenu = () => {
 
     menuWrapper.appendChild(Common.createSeparator());
     menuWrapper.appendChild(Common.createTitle("Lunch Menu"));
-    menuWrapper.appendChild(createOptions());
+    menuWrapper.appendChild(createOptions(lunchOptions));
+    menuWrapper.appendChild(Common.createTitle("Dinner Menu"));
+    menuWrapper.appendChild(createOptions(dinnerOptions));
 
     contentWrapper.appendChild(menuWrapper);
 }
 
-const createOptions = () => {
+const createOptions = (menu) => {
     let optionsWrapper = document.createElement("div");
     optionsWrapper.id = "options-container"
 
-    let options = [
-        {
-            img: Pasta,
-            title: "Caponata pasta",
-            description: "Pasta Caponata is inspired by the Sicilian dish made with chopped eggplant, olives and tomatoes in a sweet and sour sauce. It's a light an easy meal that's vegan and gluten-free!"
-        },
-        {
-            img: Risotto,
-            title: "Seafood Risotto",
-            description: "Risotto is a northern Italian rice dish cooked with broth until it reaches a creamy consistency."
-        },
-        {
-            img: Ratatouille,
-            title: "Ratatouille",
-            description: "Ratatouille is a French Provençal stewed vegetable dish, originating in Nice, and sometimes referred to as ratatouille niçoise."
-        }
-    ]
-
-    options.forEach((op) => {
+    menu.forEach((op) => {
         optionsWrapper.appendChild(createCard(op.img, op.title, op.description))
     });
 
